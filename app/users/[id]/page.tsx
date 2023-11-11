aec5ce10f82d163bc6f92b99a11e62ef76b876f9
Ivan Volos (new) <ivan.volos1@gmail.com>
2023-11-11 13:12:36 +0200

    Add a user profile page, loading and error handler pages for users route

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,33 @@
+import {prisma} from "@/lib/prisma";
+import {Metadata} from "next";
+
+interface Props {
+	params: {
+		id: string
+	}
+}
+
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+	const user = await prisma.user.findUnique({where: {id: params.id}})
+	return {title: `User profile of ${user?.name}`}
+}
+
+export default async function UserProfile({params}: Props) {
+	const user = await prisma.user.findUnique({where: {id: params.id}})
+	const {name, bio, image} = user ?? {}
+
+	return (
+		<div>
+			<h1>{name}</h1>
+
+			<img
+				width={300}
+				src={image ?? '/default_avatar.jpg'}
+				alt={`${name}'s profile`}
+			/>
+
+			<h3>Bio</h3>
+			<p>{bio}</p>
+		</div>
+	)
+}
